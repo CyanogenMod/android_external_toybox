716fb9c86057fb79a5dd7da84a6f2e433c4804bd
Steve Kondik <steve@cyngn.com>
2015-11-18 15:06:46 -0800

    Add a new "resize" command
     * This resets the terminal to the maximum possible size.
    
    Change-Id: Ifb00904bf7ddb7a042ef9b7fe9dc17df6ac1c1c2

---
+++ toys/pending/resize.c
@@ -0,0 +1,39 @@
+/* resize.c - Set maximum terminal width and height
+ *
+ *
+
+USE_RESIZE(NEWTOY(resize, "c#<1>63sw", TOYFLAG_BIN|TOYFLAG_NEEDROOT))
+
+config RESIZE
+  bool "resize"
+  default n
+  help
+    usage: resize
+
+    reset terminal to maximum width and height
+
+*/
+
+#define FOR_resize
+#include "toys.h"
+
+void resize_main(void)
+{
+  struct winsize w = { 0, 0, 0, 0 };
+  struct termios *old = (struct termios *)toybuf;
+  struct termios new;
+
+  tcgetattr(2, old);
+  memcpy(&new, old, sizeof(new));
+  new.c_cflag |= (CLOCAL | CREAD);
+  new.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+  tcsetattr(2, TCSANOW, &new);
+
+  // set cursor to 999,999, read back where it landed
+  fprintf(stderr, "\0337\033[r\033[999;999H\033[6n");
+  scanf("\033[%hu;%huR", &w.ws_row, &w.ws_col);
+  fprintf(stderr, "\0338");
+
+  ioctl(2, TIOCSWINSZ, &w);
+  tcsetattr(2, TCSANOW, old);
+}
