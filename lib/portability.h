940e0f42272aea3396d7c6dc34cc84de034f7b1b
Christopher N. Hesse <raymanfx@gmail.com>
2016-01-01 17:14:59 +0100

    portability: Add a local copy of sysinfo struct for Darwin
    Change-Id: Ifc9757f90d7707a62c680006fd2030e3c93d9749

--- lib/portability.h
+++ lib/portability.h
@@ -267,6 +267,25 @@ typedef float FLOAT;
 pid_t xfork(void);
 #endif
 
+#ifdef __APPLE__
+struct sysinfo {
+  long uptime;			/* Seconds since boot */
+  unsigned long loads[3];		/* 1, 5, and 15 minute load averages */
+  unsigned long totalram;		/* Total usable main memory size */
+  unsigned long freeram;		/* Available memory size */
+  unsigned long sharedram;	/* Amount of shared memory */
+  unsigned long bufferram;	/* Memory used by buffers */
+  unsigned long totalswap;	/* Total swap space size */
+  unsigned long freeswap;		/* swap space still available */
+  unsigned short procs;		/* Number of current processes */
+  unsigned short pad;			/* Padding needed for m68k */
+  unsigned long totalhigh;	/* Total high memory size */
+  unsigned long freehigh;		/* Available high memory size */
+  unsigned int mem_unit;		/* Memory unit size in bytes */
+  char _f[20-2*sizeof(long)-sizeof(int)];	/* Padding: libc5 uses this.. */
+};
+#endif
+
 //#define strncpy(...) @@strncpyisbadmmkay@@
 //#define strncat(...) @@strncatisbadmmkay@@
 
