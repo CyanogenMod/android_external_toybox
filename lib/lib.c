5849efb428103bb3d906747f23b32dd621be96cc
Elliott Hughes <enh@google.com>
2016-01-08 19:13:38 -0600

    Fix cat -.
    Commit 4b4ab6a50998 broke loopfiles' handling of "-".
    This broke the existing cat tests, so no new test is necessary here.
    
    Change-Id: Ic1f35c4519d47af3deb08bcd95522a3dc022548c
    Ticket: CYNGNOS-1682

--- lib/lib.c
+++ lib/lib.c
@@ -524,12 +524,12 @@ void loopfiles_rw(char **argv, int flags, int permissions, int failok,
     // Inability to open a file prints a warning, but doesn't exit.
 
     if (!strcmp(*argv, "-")) fd=0;
-    else if (0>(fd = open(*argv, flags, permissions)) && !failok)
+    else if (0>(fd = open(*argv, flags, permissions)) && !failok) {
       perror_msg("%s", *argv);
-    else {
-      function(fd, *argv);
-      if (flags & O_CLOEXEC) close(fd);
+      continue;
     }
+    function(fd, *argv);
+    if (flags & O_CLOEXEC) close(fd);
   } while (*++argv);
 }
 
