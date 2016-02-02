32fb587dc9067c99b60e63355d6725779f5428cf
Scott Mertz <scott@cyngn.com>
2016-02-02 14:40:31 -0800

    Don't exit with failure code when cp ownership preservation fails
    GNU coreutils cp explicitely states that '-a' option does not print
    or return an error if preserving ownership fails.  It's less clear
    about when using the '--preserve=ownership', but investigation shows
    it swallows the error as well.
    
    OPO-358
    Change-Id: Ibb7c94fbf553a178b3f88a724ef6f737275a4205

--- toys/posix/cp.c
+++ toys/posix/cp.c
@@ -335,12 +335,8 @@ int cp_node(struct dirtree *try)
         rc = fchownat(cfd, catch, try->st.st_uid, try->st.st_gid,
                       AT_SYMLINK_NOFOLLOW);
       else rc = fchown(fdout, try->st.st_uid, try->st.st_gid);
-      if (rc) {
-        char *pp;
-
-        perror_msg("chown '%s'", pp = dirtree_path(try, 0));
-        free(pp);
-      }
+      if (rc)
+        err = 0;
     }
 
     // timestamp
