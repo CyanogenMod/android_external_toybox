5fa8d8398eb3a8ca1e60d17ba7456ef65c4301d5
Tom Marshall <tdm@cyngn.com>
2016-02-09 10:34:55 -0800

    toybox: tar: Fix support for long names
    Move the code to set hdr.name down to where the longname check is
    performed so that toybox doesn't bail in xstrncpy().
    
    Change-Id: Id025891993746889564b479e5185cf9721b54a55

--- toys/pending/tar.c
+++ toys/pending/tar.c
@@ -190,7 +190,6 @@ static void add_file(struct archive_handler *tar, char **nam, struct stat *st)
   }
 
   memset(&hdr, 0, sizeof(hdr));
-  xstrncpy(hdr.name, hname, sizeof(hdr.name));
   itoo(hdr.mode, sizeof(hdr.mode), st->st_mode &07777);
   itoo(hdr.uid, sizeof(hdr.uid), st->st_uid);
   itoo(hdr.gid, sizeof(hdr.gid), st->st_gid);
@@ -235,7 +234,9 @@ static void add_file(struct archive_handler *tar, char **nam, struct stat *st)
     return;
   }
   if (strlen(hname) > sizeof(hdr.name))
-          write_longname(tar, hname, 'L'); //write longname NAME
+    write_longname(tar, hname, 'L'); //write longname NAME
+  else
+    xstrncpy(hdr.name, hname, sizeof(hdr.name));
   strcpy(hdr.magic, "ustar  ");
   if ((pw = getpwuid(st->st_uid)))
     snprintf(hdr.uname, sizeof(hdr.uname), "%s", pw->pw_name);
